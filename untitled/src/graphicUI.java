7380b1d9b2368fc0f9f59c7e08c96d8f910c1323
Geovanny Garcia Downing <geogd.712@estudiantec.cr>
2022-11-18 00:32:48 -0600

    Random floor creation and assets ready
    Haven't tested moving floors.
    Need a colision function
    Need a movement function for NPC
    Need key events for player

--- untitled/src/graphicUI.java
+++ untitled/src/graphicUI.java
@@ -18,23 +18,34 @@ public class graphicUI extends JFrame implements KeyListener {
     public int WIDTH = BLOCKS*DEFAULTSIZE;
     public int GROUND = HEIGHT - 2*DEFAULTSIZE;
     public int SECONDFLOOR = GROUND - SPACING*DEFAULTSIZE;
+    public int SUBSECOND = SECONDFLOOR + DEFAULTSIZE;
     public int THIRDFLOOR = SECONDFLOOR - SPACING*DEFAULTSIZE;
+    public int SUBTHIRD = THIRDFLOOR + DEFAULTSIZE;
     public int FOURTHFLOOR = THIRDFLOOR - SPACING*DEFAULTSIZE;
+    public int SUBFOURTH = FOURTHFLOOR + DEFAULTSIZE;
     public int FIFTHFLOOR = FOURTHFLOOR - SPACING*DEFAULTSIZE;
-    public int SIXTHFLOOR = FIFTHFLOOR - SPACING*DEFAULTSIZE;
+    public int SUBFIFTH = FIFTHFLOOR + DEFAULTSIZE;
     public JLabel[] ground;
     public JLabel[] secondFloor;
+    public JLabel[] subSecond;
     public JLabel[] thirdFloor;
+    public JLabel[] subThird;
     public JLabel[] fourthFloor;
+    public JLabel[] subFourth;
     public JLabel[] fifthFloor;
-    public JLabel[] sixthFloor;
+    public JLabel[] subFifth;
     public JPanel panel;
+    JLabel backgroundLabel;
+    public BufferedImage backgroundImage;
     public BufferedImage iceIcon;
+    public BufferedImage sealIcon;
+    public BufferedImage birdIcon;
+    public BufferedImage playerIcon;
     public String background = "#5c5b85";
 
     Font font = new Font("Arial", Font.PLAIN, 16);
 
-    public graphicUI(){
+    public graphicUI() {
         this.setTitle("Ice Climber");
         this.setVisible(true);
         this.setSize(WIDTH, HEIGHT);
@@ -46,16 +57,45 @@ public class graphicUI extends JFrame implements KeyListener {
         this.getContentPane().add(this.panel);
 
         try {
+            backgroundImage = ImageIO.read(getClass().getClassLoader().getResource("assets/background.png"));
             iceIcon = ImageIO.read(getClass().getClassLoader().getResource("assets/ice.png"));
+            sealIcon = ImageIO.read(getClass().getClassLoader().getResource("assets/seall.png"));
+            birdIcon = ImageIO.read(getClass().getClassLoader().getResource("assets/birdl.png"));
+            playerIcon = ImageIO.read(getClass().getClassLoader().getResource("assets/right3.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
+        JLabel newSeal = new JLabel(new ImageIcon(sealIcon));
+        newSeal.setSize(DEFAULTSIZE, DEFAULTSIZE);
+        newSeal.setLocation(0, FOURTHFLOOR - DEFAULTSIZE + SPACING);
+        panel.add(newSeal);
 
-        secondFloor = buildFloor(SECONDFLOOR);
-        thirdFloor = buildFloor(THIRDFLOOR);
-        fourthFloor = buildFloor(FOURTHFLOOR);
-        fifthFloor = buildFloor(FIFTHFLOOR);
-        sixthFloor = buildFloor(SIXTHFLOOR);
+        JLabel newPlayer = new JLabel(new ImageIcon(playerIcon));
+        newPlayer.setSize(DEFAULTSIZE, 2 * DEFAULTSIZE);
+        newPlayer.setLocation(0, GROUND - 2 * DEFAULTSIZE + SPACING);
+        panel.add(newPlayer);
+
+        JLabel newBird = new JLabel(new ImageIcon(birdIcon));
+        newBird.setSize(DEFAULTSIZE, DEFAULTSIZE);
+        newBird.setLocation(0, FIFTHFLOOR - DEFAULTSIZE + SPACING);
+        panel.add(newBird);
+
+        ground = buildGround(GROUND);
+        secondFloor = buildGround(SECONDFLOOR);
+        subSecond = buildFloor(SUBSECOND);
+        thirdFloor = buildGround(THIRDFLOOR);
+        subThird = buildFloor(SUBTHIRD);
+        fourthFloor = buildGround(FOURTHFLOOR);
+        subFourth = buildFloor(SUBFOURTH);
+        fifthFloor = buildGround(FIFTHFLOOR);
+        subFifth = buildFloor(SUBFIFTH);
+
+        backgroundLabel = new JLabel(new ImageIcon(backgroundImage));
+        backgroundLabel.setSize(WIDTH, HEIGHT);
+        backgroundLabel.setLocation(0, 0);
+        panel.add(backgroundLabel);
+
+        //newFloors();
 /*
 
        if (!admin){ USAR PARA CLIENTE OBSERVADOR O JUGADOR
@@ -71,48 +111,49 @@ public class graphicUI extends JFrame implements KeyListener {
     public JLabel[] buildFloor (int floorPosition){
         Random rand = new Random();
         JLabel[] floor = new JLabel[BLOCKS];
-        int blocksToFill = BLOCKS;
-        int beginningPosition = 0;
-        int randomLength = 0;
-        while (blocksToFill > 0){
-            if (blocksToFill < 8) {
-                randomLength = BLOCKS - blocksToFill - 1;
-            } else {
-                randomLength = rand.nextInt((blocksToFill/2 + beginningPosition + 3) + 1) + 3;
-                if ((randomLength + beginningPosition) > 24) {
-                    randomLength = BLOCKS - blocksToFill - 1;
-                }
+        for (int i = 0; i < BLOCKS; i++){
+            if (rand.nextInt((1) + 1) == 1) {
+                JLabel newBlock = new JLabel(new ImageIcon(iceIcon));
+                newBlock.setSize(DEFAULTSIZE, DEFAULTSIZE);
+                newBlock.setLocation(i * DEFAULTSIZE, floorPosition);
+                panel.add(newBlock);
+                floor[i] = newBlock;
             }
-            System.out.println(randomLength);
-            for (int i = beginningPosition; i <= beginningPosition + randomLength; i++){
-                if (i < 24){
-                    JLabel newBlock = new JLabel(new ImageIcon(iceIcon));
-                    newBlock.setSize(DEFAULTSIZE, DEFAULTSIZE);
-                    newBlock.setLocation(i*DEFAULTSIZE, floorPosition);
-                    panel.add(newBlock);
-                    System.out.println(i);
-                    floor[i] = newBlock;
-                }
-            }
-            beginningPosition = beginningPosition + randomLength + 3;
-            blocksToFill = blocksToFill - beginningPosition;
+        }
+        return floor;
+    }
+
+    public JLabel[]  buildGround (int floorPosition){
+        JLabel[] floor = new JLabel[BLOCKS];
+        for (int i = 0; i < BLOCKS; i++){
+            JLabel newBlock = new JLabel(new ImageIcon(iceIcon));
+            newBlock.setSize(DEFAULTSIZE, DEFAULTSIZE);
+            newBlock.setLocation(i*DEFAULTSIZE, floorPosition);
+            panel.add(newBlock);
+            floor[i] = newBlock;
         }
         return floor;
     }
 
     public void newFloors(){
-        deleteFLoor(ground);
-        deleteFLoor(secondFloor);
-        deleteFLoor(thirdFloor);
+        deleteFloor(ground);
+        deleteFloor(secondFloor);
+        deleteFloor(subSecond);
+        deleteFloor(thirdFloor);
+        deleteFloor(subThird);
+        deleteFloor(subFourth);
         adjustYPos(GROUND, fourthFloor);
         adjustYPos(SECONDFLOOR, fifthFloor);
-        adjustYPos(THIRDFLOOR, sixthFloor);
+        adjustYPos(SUBSECOND, subFifth);
         ground = fourthFloor;
         secondFloor = fifthFloor;
-        thirdFloor = sixthFloor;
-        fourthFloor = buildFloor(FOURTHFLOOR);
-        fifthFloor = buildFloor(FIFTHFLOOR);
-        sixthFloor = buildFloor(SIXTHFLOOR);
+        subSecond = subFifth;
+        thirdFloor = buildGround(THIRDFLOOR);
+        subThird = buildFloor(SUBTHIRD);
+        fourthFloor = buildGround(FOURTHFLOOR);
+        subFourth = buildFloor(SUBFOURTH);
+        fifthFloor = buildGround(FIFTHFLOOR);
+        subFifth = buildFloor(SUBFIFTH);
         this.panel.repaint();
     }
 
@@ -126,14 +167,13 @@ public class graphicUI extends JFrame implements KeyListener {
         }
     }
 
-    public void deleteFLoor(JLabel[] floor){
+    public void deleteFloor(JLabel[] floor){
         try{
             for(int i = 0; i < BLOCKS; i++){
                 this.remove(floor[i]);
                 this.revalidate();
             }
         } catch (Exception e){
-
         }
     }
 
@@ -144,7 +184,6 @@ public class graphicUI extends JFrame implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent keyEvent) {
-        newFloors();
     }
 
     @Override
